93e4f540dd34c4afb1c805ee9b75b04686c2bddb
Scissordragonboy <Scissordragonboy1@gmail.com>
2020-01-11 01:56:37 +0000

    oxygen: camera: drop gcam * build camera.msm8953 instead of Gcam

--- device.mk
+++ device.mk
@@ -129,7 +129,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
     libmm-qcamera \
-    Gcam \
+    camera.msm8953 \
     Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
